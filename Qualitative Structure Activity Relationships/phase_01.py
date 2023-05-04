67d9210ead0ba0b1589b9b48a280d3906b0847a3
Ahmed <ahmaddeebdev@gmail.com>
2023-05-04 16:07:33 +0300

    [0S00]-[Adding DS]

---
+++ Qualitative Structure Activity Relationships/phase_01.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+
+#Step 00: Loading data
+
+# INFO
+"""
+Abstract: Two sets of datasets are given: pyrimidines and triazines
+
+Data Set Characteristics:  
+
+Domain-Theory
+
+Number of Instances:
+
+N/A
+
+Area:
+
+Physical
+
+Attribute Characteristics:
+
+N/A
+
+Number of Attributes:
+
+N/A
+
+Date Donated
+
+N/A
+
+Associated Tasks:
+
+N/A
+
+Missing Values?
+
+N/A
+
+Number of Web Hits:
+
+32205
+
+
+Source:
+
+Ross D. King
+Biomolecular Modelling Laboratory
+Imperial Cancer Research Fund
+P.O. Box 123
+44 Lincoln's Inn Fields
+London WC2A 3PX
+U.K.
++44-71-242-0200 x3023
+rd_king '@' icrf.ac.uk
+
+
+Data Set Information:
+
+N/A
+
+
+Attribute Information:
+
+N/A
+
+
+Relevant Papers:
+
+N/A
+
+
+
+Citation Request:
+
+Please refer to the Machine Learning Repository's citation policy
+"""
