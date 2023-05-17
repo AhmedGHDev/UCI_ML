0822e41c82ceaad6ac43681862d9cae5b3131b07
Ahmed <ahmaddeebdev@gmail.com>
2023-05-17 17:14:40 +0300

    [0S00]-[Adding DS]

---
+++ OpinRank Review Dataset/phase_01.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+
+#Step 00: Loading data
+
+# INFO
+"""
+Abstract: This data set contains user reviews of cars and and hotels collected from Tripadvisor (~259,000 reviews) and Edmunds (~42,230 reviews).
+
+Data Set Characteristics:  
+
+Text
+
+Number of Instances:
+
+N/A
+
+Area:
+
+Computer
+
+Attribute Characteristics:
+
+N/A
+
+Number of Attributes:
+
+N/A
+
+Date Donated
+
+2011-07-26
+
+Associated Tasks:
+
+N/A
+
+Missing Values?
+
+N/A
+
+Number of Web Hits:
+
+65146
+
+
+Source:
+
+Kavita Ganesan & ChengXiang Zhai
+University of Illinois @ Urbana Champaign
+http://www.kavita-ganesan.com/entity-ranking-data
+
+
+Data Set Information:
+
+Car Reviews
+------------
+-Full reviews of cars for model-years 2007, 2008, and 2009
+-There are about 140-250 cars for each model year
+-Extracted fields include dates, author names, favorites and the full textual review
+-Total number of reviews: ~42,230
+
+Hotel Reviews
+--------------
+-Full reviews of hotels in 10 different cities (Dubai, Beijing, London, New York City, New Delhi, San Francisco, Shanghai, Montreal, Las Vegas, Chicago)
+-There are about 80-700 hotels in each city
+-Extracted fields include date, review title and the full review
+-Total number of reviews: ~259,000
+
+
+Attribute Information:
+
+N/A
+
+
+Relevant Papers:
+
+Ganesan, K. A., and C. X. Zhai, 'Opinion-Based Entity Ranking', Information Retrieval, 2011.
+
+
+
+Citation Request:
+
+Bibtex as follows:
+
+@article {opinrank,
+title = {Opinion-Based Entity Ranking},
+journal = {Information Retrieval},
+year = {2011},
+keywords = {adhoc multifaceted search, entity oriented search, entity ranking, entity retrieval, product search},
+doi = {10.1007/s10791-011-9174-8},
+attachments = {[Web Link]},
+author = {Kavita Ganesan and ChengXiang Zhai}
+}
+"""
